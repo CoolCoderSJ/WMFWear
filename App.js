73a1cb55fabdf081f85795df356fa3038c89bab7
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2024-07-31 11:31:20 -0400

    fix sessions, add home page, start flight page

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import { Client, Account, ID, Databases } from 'react-native-appwrite';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import React from 'react';
+import * as React from 'react';
 import Toast, { BaseToast, ErrorToast, InfoToast } from 'react-native-toast-message';
 import RNRestart from 'react-native-restart';
-
+import axios from 'axios';
 
 const setObj = async (key, value) => { try { const jsonValue = JSON.stringify(value); await AsyncStorage.setItem(key, jsonValue) } catch (e) { console.log(e) } }
 const setPlain = async (key, value) => { try { await AsyncStorage.setItem(key, value) } catch (e) { console.log(e) } }
@@ -29,12 +29,20 @@ client
   .setProject('wheresmyflight')
   .setPlatform('dev.shuchir.wmfwear');
 
+get('sessToken').then((value) => {
+  console.log(value)
+  client.setSession(value);
+})
+
 account = new Account(client);
 db = new Databases(client);
+
+let flights = [];
 
 export default function App() {
   const [, forceUpdate] = React.useReducer(x => x + 1, 0);
   const [form, setForm] = React.useState(null);
+  const [flightPage, setFlightPage] = React.useState(null);
 
   React.useEffect(() => {
     get('sessId').then((value) => {
@@ -90,15 +98,39 @@ export default function App() {
     catch {}
 
     try { 
-      account.createEmailPasswordSession(email, password)
-      .then((res) => {
-        account.get().then(details => {
-          console.log("DETAILS", details)
-          setPlain("sessId", details['$id'])
-          sessId = details['$id']
-          RNRestart.restart();
-        })
-      })
+      let data = JSON.stringify({
+        "email": email,
+        "password": password
+      });
+      
+      let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://appwrite.shuchir.dev/v1/account/sessions/email',
+        headers: { 
+          'X-Appwrite-Response-Format': '1.5.5', 
+          'X-Appwrite-Project': 'wheresmyflight', 
+          'Content-Type': 'application/json', 
+        },
+        data : data
+      };
+      
+      let res = await axios.request(config)
+      console.log(res.headers['set-cookie'])
+      let cookies = res.headers['set-cookie']
+      let session = cookies[0].split(";")[0]
+      let key = session.split("=")[0]
+      let value = session.split("=")[1]
+      console.log(key, value)
+
+      setPlain("sessToken", value)
+
+      client.setSession(value)
+      let details = await account.get()
+      console.log("DETAILS", details)
+      setPlain("sessId", details['$id'])
+      sessId = details['$id']
+      RNRestart.restart();
     }
     catch (err) {
       console.log(err)
@@ -113,12 +145,13 @@ export default function App() {
 
   if (sessId) {
     db.listDocuments("data", "flights").then((res) => {
-      console.log(res)
+      flights = res.documents.reverse();
+      forceUpdate();
     })
   }
 
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={{ backgroundColor: "#000" }}>
     {!sessId &&
     <View style={{...styles.container, marginTop: 10}}>
       <Text style={{...styles.text, fontSize: 20}}>Login</Text>
@@ -149,14 +182,103 @@ export default function App() {
       </TouchableOpacity>
     </View>
     }
-    {sessId &&
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome back!</Text>
-    </View>
+    {sessId && !flightPage &&
+    <ScrollView contentContainerStyle={{ paddingVertical: 50, paddingLeft: 10 }}>
+      {flights.map((flight, index) => {
+        let dep, arr, schedDep, schedArr;
+
+        schedDep = new Date(Date.parse(flight.scheduledTime[0])).toUTCString().split(":00 GMT ")[0].split(" ")[4].slice(0, 5)
+        schedArr = new Date(Date.parse(flight.scheduledTime[1])).toUTCString().split(":00 GMT ")[0].split(" ")[4].slice(0, 5)
+        if (flight.time[0]) dep = new Date(Date.parse(flight.time[0])).toUTCString().split(":00 GMT ")[0].split(" ")[4].slice(0, 5)
+        if (flight.time[1]) arr = new Date(Date.parse(flight.time[1])).toUTCString().split(":00 GMT ")[0].split(" ")[4].slice(0, 5)
+        if (flight.scheduledTime[0] && !flight.time[0]) { flight.time[0] = flight.scheduledTime[0]; dep = schedDep }
+        if (flight.scheduledTime[1] && !flight.time[1]) { flight.time[1] = flight.scheduledTime[1]; arr = schedArr }
+
+        function tConvert (time) {
+          // Check correct time format and split into components
+          time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+  
+          if (time.length > 1) { // If time format correct
+            time = time.slice (1);  // Remove full string match value
+            time[5] = +time[0] < 12 ? ' AM' : ' PM'; // Set AM/PM
+            time[0] = +time[0] % 12 || 12; // Adjust hours
+          }
+          return time.join (''); // return adjusted time or original string
+        }
+  
+        try {dep = tConvert(dep)}
+        catch {console.log("could not get tz for dep")}
+        try {arr = tConvert(arr)}
+        catch {console.log("could not get tz for arr")} 
+        try {schedDep = tConvert(schedDep)}
+        catch {console.log("could not get tz for scheddep")}
+        try {schedArr = tConvert(schedArr)}
+        catch {console.log("could not get tz for schedarr")}
+
+        
+        let depDiffNum = (new Date(Date.parse(flight.time[0])).getTime() - new Date(Date.parse(flight.scheduledTime[0])).getTime()) / 60000;
+        let depDiffText = ""
+        if (depDiffNum < 0) {
+          depDiffNum *= -1;
+          depDiffText = "early"
+        }
+        else {
+          depDiffText = "late"
+        }
+        let depDiff = depDiffNum%60 + "min";
+        if (Math.floor(depDiffNum/60) > 0) {
+          depDiff = Math.floor(depDiffNum/60) + "hr " + depDiff
+        }
+        if (depDiffNum == 0) {
+          depDiff = "on time"
+          depDiffText = ""
+        }
+  
+        let arrDiffNum = (new Date(Date.parse(flight.time[1])).getTime() - new Date(Date.parse(flight.scheduledTime[1])).getTime()) / 60000;
+        let arrDiffText = ""
+        if (arrDiffNum < 0) {
+          arrDiffNum *= -1;
+          arrDiffText = "early"
+        }
+        else {
+          arrDiffText = "late"
+        }
+        let arrDiff = arrDiffNum%60 + "min";
+        if (Math.floor(arrDiffNum/60) > 0) {
+          arrDiff = Math.floor(arrDiffNum/60) + "hr " + arrDiff
+        }
+        if (arrDiffNum == 0) {
+          arrDiff = "on time"
+          arrDiffText = ""
+        }
+
+        return (
+          <TouchableOpacity style={styles.outer}>
+            <View style={styles.left}>
+              <Text style={styles.text}>{flight.flightId}</Text>
+              <Text style={{...styles.text, ...styles.bigger}}>{flight.airport[0]}</Text>
+              <Text style={{...styles.text, color: depDiffText == 'late' ? 'red' : '#90EE90'}}>{dep}</Text>
+              <Text style={{...styles.text, ...styles.smaller}}>{flight.gate[0] ? 'Gate '+flight.gate[0] : ""}</Text>
+            </View>
+            <View style={styles.right}>
+              <Text style={{...styles.text, textAlign: "right"}}>{new Date(flight.scheduledTime[0].split("+")[0]).toDateString().split(" ").splice(1,2).join(" ")}</Text>
+              <Text style={{...styles.text, ...styles.bigger, textAlign: "right"}}>{flight.airport[1]}</Text>
+              <Text style={{...styles.text, color: arrDiffText == 'late' ? 'red' : '#90EE90', textAlign: "right"}}>{arr}</Text>
+              <Text style={{...styles.text, ...styles.smaller, textAlign: "right"}}>{flight.gate[1] ? 'Gate '+flight.gate[1] : ""}</Text>
+            </View>
+          </TouchableOpacity>
+        )
+      })}
+    </ScrollView>
     }
 
+      {sessId && flightPage &&
+          <ScrollView contentContainerStyle={{ paddingVertical: 50, paddingLeft: 10 }}>
+          </ScrollView>
+      }
+
     <Toast config={toastConfig} />
-    </View>
+    </ScrollView>
   );
 }
 
@@ -192,5 +314,33 @@ const styles = StyleSheet.create({
     width: 150,
     alignItems: "center",
     marginVertical: 5,
+  },
+
+  outer: {
+    padding: 10,
+    borderRadius: 8,
+    width: 175,
+    alignItems: "center",
+    marginVertical: 5,
+    flexDirection: "row"
+  },
+
+  left: {
+    flex: 1
+  },
+
+  right: {
+    flex: 1,
+    textAlign: "right"
+  },
+
+  bigger: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+
+  smaller: {
+    fontSize: 12
   }
+
 });
